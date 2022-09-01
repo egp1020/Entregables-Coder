adb96cb6d5490d844e0ec82e7d1c5552e4b55846
Estefanía Garcés Pérez <47468501+egp1020@users.noreply.github.com>
2022-08-31 19:14:48 -0500

    Update exercise-3.py

--- Tercer entregable/exercise-3.py
+++ Tercer entregable/exercise-3.py
@@ -16,3 +16,23 @@ for n in range(1, 101, 2):
 # Add up the list
 list = sum(list)
 print(list)
+
+# ------------------------ · Solution 2 · ------------------------
+
+# Initializing variables.
+_sum = 0
+
+# Generating cycle.
+for i in range(100):
+    if i % 2 != 0:
+        _sum += i
+
+# Showing result.
+print(f"La sumatoria es {_sum}.")
+
+# ------------------------ · Solution 3 · ------------------------
+
+suma = sum(range(1, 100, 2))
+
+# Showing result.
+print(f"La sumatoria es {suma}.")
